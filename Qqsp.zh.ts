e625b136527d55faed4a320de880289838f2b07f
cskungfu <57865347+cskungfu@users.noreply.github.com>
2025-10-04 06:41:48 +0800

    Add files via upload

---
+++ Qqsp.zh.ts
@@ -0,0 +1,361 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="68"/>
+        <source>ToolBar</source>
+        <translation type="unfinished">工具栏</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="358"/>
+        <source>Error</source>
+        <translation type="unfinished">错误</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="586"/>
+        <source>&amp;Quest</source>
+        <translation type="unfinished">任务(&amp;Q)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="589"/>
+        <source>Open game...</source>
+        <translation type="unfinished">打开游戏...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="594"/>
+        <source>Restart game</source>
+        <translation type="unfinished">重新开始游戏</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="602"/>
+        <source>Exit</source>
+        <translation type="unfinished">退出</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="608"/>
+        <source>&amp;Game</source>
+        <translation type="unfinished">游戏(&amp;G)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="611"/>
+        <source>Open saved game...</source>
+        <translation type="unfinished">打开已保存的游戏...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="615"/>
+        <source>Save game...</source>
+        <translation type="unfinished">保存游戏...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="619"/>
+        <source>Quick Load</source>
+        <translation type="unfinished">快速加载</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="623"/>
+        <source>Quick Save</source>
+        <translation type="unfinished">快速保存</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="629"/>
+        <source>&amp;Settings</source>
+        <translation type="unfinished">设置(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="632"/>
+        <source>Show / Hide</source>
+        <translation type="unfinished">显示 / 隐藏</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="667"/>
+        <source>Captions</source>
+        <translation type="unfinished">字幕</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="693"/>
+        <source>Hotkeys for actions</source>
+        <translation type="unfinished">动作热键</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="700"/>
+        <source>Window / Fullscreen mode</source>
+        <translation type="unfinished">窗口 / 全屏模式</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="708"/>
+        <source>Display HTML code as plain text</source>
+        <translation type="unfinished">将HTML代码显示为纯文本</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="719"/>
+        <source>Options...</source>
+        <translation type="unfinished">选项...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="725"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished">帮助(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="728"/>
+        <source>About...</source>
+        <translation type="unfinished">关于...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="755"/>
+        <source>Main desc</source>
+        <translation type="unfinished">主描述</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="761"/>
+        <source>Objects</source>
+        <translation type="unfinished">对象</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="773"/>
+        <source>Actions</source>
+        <translation type="unfinished">动作</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="786"/>
+        <source>Additional desc</source>
+        <translation type="unfinished">附加描述</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="811"/>
+        <source>Input area</source>
+        <translation type="unfinished">输入区域</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="822"/>
+        <source>Image</source>
+        <translation type="unfinished">图像</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1068"/>
+        <location filename="mainwindow.cpp" line="1070"/>
+        <location filename="mainwindow.cpp" line="1078"/>
+        <source>Select game file</source>
+        <translation type="unfinished">选择游戏文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1068"/>
+        <location filename="mainwindow.cpp" line="1070"/>
+        <location filename="mainwindow.cpp" line="1078"/>
+        <source>QSP games (*.qsp *.gam)</source>
+        <translation type="unfinished">QSP游戏 (*.qsp *.gam)</translation>
+    </message>
+    <message>
+        <location filename="callbacks_gui.cpp" line="296"/>
+        <source>Info</source>
+        <translation type="unfinished">信息</translation>
+    </message>
+    <message>
+        <location filename="callbacks_gui.cpp" line="351"/>
+        <source>Input data</source>
+        <translation type="unfinished">输入数据</translation>
+    </message>
+    <message>
+        <location filename="callbacks_gui.cpp" line="394"/>
+        <location filename="mainwindow.cpp" line="1112"/>
+        <location filename="mainwindow.cpp" line="1114"/>
+        <source>Select saved game file</source>
+        <translation type="unfinished">选择已保存的游戏文件</translation>
+    </message>
+    <message>
+        <location filename="callbacks_gui.cpp" line="394"/>
+        <location filename="callbacks_gui.cpp" line="412"/>
+        <location filename="mainwindow.cpp" line="1112"/>
+        <location filename="mainwindow.cpp" line="1114"/>
+        <location filename="mainwindow.cpp" line="1131"/>
+        <location filename="mainwindow.cpp" line="1133"/>
+        <source>Saved game files (*.sav)</source>
+        <translation type="unfinished">已保存的游戏文件 (*.sav)</translation>
+    </message>
+    <message>
+        <location filename="callbacks_gui.cpp" line="412"/>
+        <location filename="mainwindow.cpp" line="1131"/>
+        <location filename="mainwindow.cpp" line="1133"/>
+        <source>Select file to save</source>
+        <translation type="unfinished">选择要保存的文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1199"/>
+        <source>, </source>
+        <translation type="unfinished">，</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1200"/>
+        <source>&lt;h2&gt;Qqsp&lt;/h2&gt;&lt;p&gt;Copyright &amp;copy; 2017-2019, Sonnix&lt;/p&gt;</source>
+        <translation type="unfinished">&lt;h2&gt;Qqsp&lt;/h2&gt;&lt;p&gt;版权所有 &amp;copy; 2017-2019, Sonnix&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1202"/>
+        <source>&lt;p&gt;Application version: %1&lt;br&gt;QSP library version: %2&lt;br&gt;Qt library version: %3&lt;br&gt;Application compilation date: %4&lt;br&gt;Library compilation date: %5&lt;/p&gt;</source>
+        <oldsource>&lt;p&gt;Application version: %1&lt;br&gt;QSP library version: %2&lt;br&gt;Application compilation date: %3&lt;br&gt;Library compilation date: %4&lt;/p&gt;</oldsource>
+        <translation type="unfinished">&lt;p&gt;应用程序版本: %1&lt;br&gt;QSP库版本: %2&lt;br&gt;Qt库版本: %3&lt;br&gt;应用程序编译日期: %4&lt;br&gt;库编译日期: %5&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1203"/>
+        <source>About</source>
+        <translation type="unfinished">关于</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsDialog</name>
+    <message>
+        <location filename="optionsdialog.ui" line="17"/>
+        <source>Options</source>
+        <translation type="unfinished">选项</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="28"/>
+        <source>Use custom font size</source>
+        <translation type="unfinished">使用自定义字体大小</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="42"/>
+        <source>Use custom font</source>
+        <translation type="unfinished">使用自定义字体</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="71"/>
+        <location filename="optionsdialog.ui" line="127"/>
+        <location filename="optionsdialog.ui" line="207"/>
+        <location filename="optionsdialog.ui" line="256"/>
+        <source>Select</source>
+        <translation type="unfinished">选择</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="80"/>
+        <source>Custom background color</source>
+        <translation type="unfinished">自定义背景颜色</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="136"/>
+        <source>Language</source>
+        <translation type="unfinished">语言</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="146"/>
+        <source>Custom text color</source>
+        <translation type="unfinished">自定义文本颜色</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="153"/>
+        <source>Custom link color</source>
+        <translation type="unfinished">自定义链接颜色</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="160"/>
+        <source>Autostart last game</source>
+        <translation type="unfinished">自动启动上次游戏</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="265"/>
+        <source>Separate config per game</source>
+        <translation type="unfinished">每个游戏单独配置</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="272"/>
+        <source>Sound volume</source>
+        <translation type="unfinished">音量</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="289"/>
+        <source>Disable video</source>
+        <translation type="unfinished">禁用视频</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="296"/>
+        <source>Force autoplay and loop for video</source>
+        <translation type="unfinished">强制视频自动播放和循环</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="303"/>
+        <source>HTML5 Extras (experimental)</source>
+        <translation type="unfinished">HTML5扩展功能（实验性）</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="343"/>
+        <source>Cancel</source>
+        <translation type="unfinished">取消</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.ui" line="350"/>
+        <source>Ok</source>
+        <translation type="unfinished">确定</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="24"/>
+        <source>__LANGNAME__</source>
+        <translation>中文</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="25"/>
+        <location filename="mainwindow.cpp" line="113"/>
+        <source>__LANGID__</source>
+        <translation>zh_CN</translation>
+    </message>
+</context>
+<context>
+    <name>QspExecWebEngineUrlSchemeHandler</name>
+    <message>
+        <location filename="qspexecwebengineurlschemehandler.cpp" line="52"/>
+        <source>Error</source>
+        <translation type="unfinished">错误</translation>
+    </message>
+</context>
+<context>
+    <name>QspImgCanvas</name>
+    <message>
+        <location filename="qspimgcanvas.cpp" line="9"/>
+        <source>Image</source>
+        <translation type="unfinished">图像</translation>
+    </message>
+</context>
+<context>
+    <name>QspMsgDlg</name>
+    <message>
+        <location filename="qspmsgdlg.cpp" line="32"/>
+        <source>OK</source>
+        <translation type="unfinished">确定</translation>
+    </message>
+</context>
+<context>
+    <name>QspWebChannel</name>
+    <message>
+        <location filename="qspwebchannel.cpp" line="38"/>
+        <source>Error</source>
+        <translation type="unfinished">错误</translation>
+    </message>
+</context>
+<context>
+    <name>__LANG__</name>
+    <message>
+        <location filename="optionsdialog.cpp" line="50"/>
+        <location filename="optionsdialog.cpp" line="59"/>
+        <source>__LANGNAME__</source>
+        <translation>中文</translation>
+    </message>
+    <message>
+        <location filename="optionsdialog.cpp" line="50"/>
+        <location filename="optionsdialog.cpp" line="59"/>
+        <source>__LANGID__</source>
+        <translation>zh_CN</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.cpp" line="49"/>
+        <source>Game file to open.</source>
+        <translation type="unfinished">要打开的游戏文件。</translation>
+    </message>
+</context>
+</TS>
